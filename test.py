02c8dbeebb62e9278f933b042e2789af3cfde4b1
Shawn Roche <sroche0@gmail.com>
2017-10-20 10:20:48 -0400

    added playerscore function

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ from pprint import pprint
 
 c = mflpyapi.Client()
 
-c.update_data_cache()
-players = c.player.dump_player_data()
+# c.update_data_cache()
+players = c.player.player_scores()
+print(players)
 
-for i in players['result']:
-    if i['position'] == 'WR':
-        print(sorted(i))
+for i in players['result']['playerscore']:
+    print(i)
 
 # c.roster.league = 60050
 # resp = c.roster.get_rosters(12)
